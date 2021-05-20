533c57712c574b46b2917346732b63da72ba4ea3
Tejas Kumar D P (Trianz) <tejas.kumardp@trianz.com>
2021-05-20 17:59:39 +0530

    Added convertion Hour to mins & secs

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 steps:
 1. check all input parameter are valid or not
+2. converting hours to minutes and seconds
--- index.js
+++ index.js
@@ -6,6 +6,17 @@ const timeconvertion = (val ,fromUnit ,toUnit) =>
         return 'Value should be greater than 0';
     if(fromUnit == toUnit)
         return 'Units are same';
+         //hours to minutes and seconds
+    if(fromUnit == 'hours'){
+        //to minutes
+        if(toUnit == 'minutes') 
+            return val * 60 + ' minutes';
+        //to seconds
+        else if(toUnit == 'seconds')
+            return val * 3600 + ' seconds';
+        else
+            return 'invalid o/p unit';
+    }
 }
 
 var result = timeconvertion(10, 'seconds','seconds');
