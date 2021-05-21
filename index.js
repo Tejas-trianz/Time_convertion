ed53f32f681d2800ccba616e3d624507e6f33bb5
Tejas Kumar D P (Trianz) <tejas.kumardp@trianz.com>
2021-05-21 18:36:32 +0530

    modified if condition for checking inputs

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
-const timeconvertion = (val ,fromUnit ,toUnit) => 
-{
-    if(!parseInt(val))
+const timeconvertion = (val, fromUnit, toUnit) => {
+    if (typeof (val) !== 'number')
         return 'Value error';
-    if(val < 0)
+    if (val <= 0)
         return 'Value should be greater than 0';
-    if(fromUnit == toUnit)
+    if (fromUnit == toUnit)
         return 'Units are same';
 
     //hours to minutes and seconds
-    if(fromUnit == 'hours'){
+    if (fromUnit == 'hours') {
         //to minutes
-        if(toUnit == 'minutes') 
+        if (toUnit == 'minutes')
             return val * 60 + ' minutes';
         //to seconds
-        else if(toUnit == 'seconds')
+        else if (toUnit == 'seconds')
             return val * 3600 + ' seconds';
         else
             return 'invalid o/p unit';
     }
     //minutes to seconds and hours
-    else if(fromUnit == 'minutes'){
+    else if (fromUnit == 'minutes') {
         //to seconds
-        if(toUnit == 'seconds')
+        if (toUnit == 'seconds')
             return val * 60 + ' seconds';
         //to hours
-        else if(toUnit == 'hours'){
+        else if (toUnit == 'hours') {
             let calhrs = Math.floor(val / 60);
             let calmins = val % 60;
             return calhrs + ':' + calmins + ' hours';
@@ -33,15 +32,15 @@ const timeconvertion = (val ,fromUnit ,toUnit) =>
             return 'invalid o/p unit';
     }
     //seconds to hours and minutes
-    else if(fromUnit == 'seconds'){
+    else if (fromUnit == 'seconds') {
         //to minutes
-        if(toUnit == 'minutes'){
+        if (toUnit == 'minutes') {
             let calmins = Math.floor(val / 60);
             let calsecs = (val % 60);
             return calmins + ':' + calsecs + ' minutes';
         }
         //to hours
-        else if(toUnit == 'hours'){
+        else if (toUnit == 'hours') {
             let calhrs = Math.floor(val / 3600);
             let calmins = Math.floor((val % 3600) / 60);
             return calhrs + ':' + calmins + ' hours';
@@ -49,12 +48,9 @@ const timeconvertion = (val ,fromUnit ,toUnit) =>
         else
             return 'invalid o/p unit';
     }
-    else{
+    else {
         return 'invalid i/p unit';
     }
 }
-
-// var result = timeconvertion(17000, 'seconds','hours');
-// console.log(result);
 
 module.exports = timeconvertion;
