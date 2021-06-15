3b98bbb89a1bbfa5ec3a4635ddfcddc156a00079
Tejas <84391890+Tejas-trianz@users.noreply.github.com>
2021-06-15 17:58:51 +0530

    enhancement

---
+++ test/demo.test.js
@@ -0,0 +1,38 @@
+
+//examples
+
+test('null', () => {
+    const n = null;
+    expect(n).toBeNull();
+    expect(n).toBeDefined();
+    expect(n).not.toBeUndefined();
+    expect(n).not.toBeTruthy();
+    expect(n).toBeFalsy();
+  });
+
+  test('two plus two', () => {
+    const value = 2 + 2;
+    expect(value).toBeGreaterThan(3);
+    expect(value).toBeGreaterThanOrEqual(4);
+    expect(value).toBeLessThan(5);
+    expect(value).toBeLessThanOrEqual(4.5);
+  
+    // toBe and toEqual are equivalent for numbers
+    expect(value).toBe(4);
+    expect(value).toEqual(4);
+  });
+
+  test('adding floating point numbers', () => {
+    const value = 0.1 + 0.2;
+    //expect(value).toBe(0.3);           This won't work because of rounding error
+    expect(value).toBeCloseTo(0.3); // This works.
+  });
+
+  //for strings
+  test('there is no I in team', () => {
+    expect('team').not.toMatch(/I/);
+  });
+  
+  test('but there is a "stop" in Christoph', () => {
+    expect('Christoph').toMatch(/stop/);
+  });
